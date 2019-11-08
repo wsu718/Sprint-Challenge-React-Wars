4c52ea47c9c35f5919d8e12ab410b19dc7e689d2
William Sulinski <w@williamsulinski.com>
2019-11-08 11:32:42 -0800

    Added stretch

--- starwars/src/components/CharacterCard.js
+++ starwars/src/components/CharacterCard.js
@@ -7,12 +7,13 @@ const CharacterCard = (props) => {
         <div className="card" key={props.id}>
             <Card>
                 <CardImg top width="100%" src="http://www.facetheforce.today/random/400" alt="Starwars image" />
+                
                 <CardHeader><span className="wanted">Wanted:</span> {props.name}</CardHeader>
                 <CardBody>
-                    <CardText><span className="bold">Height:</span>{props.height} </CardText>
-                    <CardText><span className="bold">Weight: </span>{props.weight}</CardText>
+                    <CardText><span className="bold">Height:</span>{Math.round(props.height / 30.48 * 10)/10 } ft  </CardText>
+                    <CardText><span className="bold">Weight: </span>{Math.round(props.weight * 2.20462)} lbs</CardText>
                     <CardText><span className="bold">Eye color: </span>{props.eye_color}</CardText>
-                    <CardText><span className="bold">Hair color:</span> {props.hair_color}</CardText>
+                    <CardText><span className="bold">Hair color:</span> {props.hair_color !== 'n/a' ? props.hair_color : `Robots don't have hair!'` }</CardText>
 
                     <Button href={props.homeworld}>View Home World</Button>
                 </CardBody> 
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,24 +1,51 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import CharacterCard from './CharacterCard';
-import { Row } from 'reactstrap';
+import { Row, Col, Button } from 'reactstrap';
 
 const CharacterList = () => {
 
     const [characters, setCharacters] = useState([]);
+    const [next, setNext] = useState('');
+    const [prev, setPrev] = useState('https://swapi.co/api/people/');
+    const [url, setUrl] = useState('https://swapi.co/api/people/')
+
+    // const getNewChars = () => {
+    //     console.log('clikced next!')
+    //     console.log(next);
+    //     useEffect(() => {
+    //         axios.get(next)
+    //         .then(response => {
+    //             console.log('clikced next!')
+    //         })
+    //         .catch(error => {
+    //             console.log('No data was returned', error);
+    //         });
+    //     }, []);
+        
+    
+    // }
 
     useEffect(() => {
-        axios.get('https://swapi.co/api/people/')
+        axios.get(url)
         .then(response => {
             setCharacters(response.data.results);
+            setNext(response.data.next);
+            setPrev(response.data.previous)
         })
         .catch(error => {
             console.log('No data was returned', error);
         });
-    }, []);
+    }, [url]);
 
     return (
         <div>
+            <Row>
+                <Col>
+                <Button onClick={() => setUrl([prev]) }>Previous</Button>
+                <Button onClick={() => setUrl(next) }>Next</Button>
+                </Col>
+            </Row>
             <Row>
             {characters.map(character => {
                 return <CharacterCard 
@@ -33,9 +60,14 @@ const CharacterList = () => {
                 />
             })}
             </Row>
+            
         </div>
     );
 
+
+
 }
+
+
 
 export default CharacterList;
